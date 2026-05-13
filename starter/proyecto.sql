c5cd1c5a24f2b63211c75100e5b154e10ded4328
dilriv2104 <dilanriveros2104gmail.com>
2026-05-13 15:27:38 -0500

    ejercicio_5

--- starter/proyecto.sql
+++ starter/proyecto.sql
@@ -1,36 +1,32 @@
--- COUNT
--- Total de productos registrados
-SELECT COUNT(*) AS total_productos
-FROM productos;
 
-
--- SUM + AVG
--- Suma total y promedio de precios
 SELECT 
-    SUM(precio) AS suma_precios,
-    AVG(precio) AS promedio_precios
-FROM productos;
-
--- GROUP BY
--- Cantidad de productos y promedio de precio
--- agrupados por categoría
-SELECT 
-    categoria,
-    COUNT(*) AS cantidad_productos,
-    AVG(precio) AS promedio_precio
+    id_producto,
+    nombre,
+    precio
 FROM productos
-GROUP BY categoria;
-
-
-
--- HAVING
--- Categorías con más de 3 productos registrados
+WHERE precio BETWEEN 50000 AND 200000;
 
 
 SELECT 
-    categoria,
-    COUNT(*) AS total_productos,
-    SUM(stock) AS stock_total
+    nombre,
+    categoria
 FROM productos
-GROUP BY categoria
-HAVING COUNT(*) > 3;
+WHERE categoria IN ('Tecnología', 'Oficina', 'Accesorios');
+
+
+SELECT 
+    id_producto,
+    nombre
+FROM productos
+WHERE nombre LIKE '%mouse%';
+
+
+SELECT 
+    nombre,
+    categoria,
+    precio,
+    stock
+FROM productos
+WHERE precio BETWEEN 30000 AND 500000
+AND categoria IN ('Tecnología', 'Gaming')
+AND nombre LIKE '%pro%';
